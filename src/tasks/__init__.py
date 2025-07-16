86675d28a45b99dd197efd5368cccc0dc5bcf2d7
mehulbhardwaj <mehul.bhardwaj@outlook.com>
2025-07-16 13:23:36 +0530

    Add hierarchy tests and style fixes

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,3 +1,4 @@
 from .task_manager import TaskManager
+from .hierarchy_manager import HierarchyManager, IssueNode
 
-__all__ = ["TaskManager"]
+__all__ = ["TaskManager", "HierarchyManager", "IssueNode"]
