58432c0161cd7a8aee81954555af7a07f21df19f
mehulbhardwaj <mehul.bhardwaj@outlook.com>
2025-07-19 11:57:25 +0530

    feat: add configurable ranking engine

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,4 +1,11 @@
 from .hierarchy_manager import HierarchyManager, IssueNode
+from .ranking import RankingConfig, RankingEngine
 from .task_manager import TaskManager
 
-__all__ = ["TaskManager", "HierarchyManager", "IssueNode"]
+__all__ = [
+    "TaskManager",
+    "HierarchyManager",
+    "IssueNode",
+    "RankingConfig",
+    "RankingEngine",
+]
